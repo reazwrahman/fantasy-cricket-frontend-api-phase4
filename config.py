1dc6b24ddbd91197453d2319e1bce4d36722a582
Reaz <reaz.rahman@macaulay.cuny.edu>
2022-01-15 10:20:28 -0500

    configs will be declared in heroku platform

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = 'fantasysquad30@gmail.com' #os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = 'FantasySquad@2022' #os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[CMCC Fantasy Squad Authentication]'
     FLASKY_MAIL_SENDER = 'Fantasy Squad Admin <fantasysquad30@gmail.com>'
-    FLASKY_ADMIN = 'fantasysquad30@gmail.com' #os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
@@ -94,6 +94,5 @@ config = {
     'testing': TestingConfig,
     'production': ProductionConfig,
     'heroku': HerokuConfig,
-
     'default': DevelopmentConfig
 }
