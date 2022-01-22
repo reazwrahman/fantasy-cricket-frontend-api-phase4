6f21afcd320f70b29a52f01c62528ee82afe2a65
Reaz <reaz.rahman@macaulay.cuny.edu>
2022-01-22 10:40:27 -0500

    allowing command line access to models

--- fantasy_squad.py
+++ fantasy_squad.py
@@ -10,7 +10,7 @@ import sys
 import click
 from flask_migrate import Migrate, upgrade
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, GameDetails, SelectedSquad
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -19,7 +19,7 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role,
-                Permission=Permission)
+                GameDetails=GameDetails, SelectedSquad=SelectedSquad )
 
 
 @app.cli.command()
