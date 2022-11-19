a9201164fcb7d3ba78fea6d3c1f64783962b462c
RW Rahman <33879829+reazwrahman@users.noreply.github.com>
2022-11-19 14:30:00 -0500

    removing login creds

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = 'fantasysquad30@gmail.com' #os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = 'FantasySquad@2022' #os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = ""
+    MAIL_PASSWORD = ""
     FLASKY_MAIL_SUBJECT_PREFIX = '[CMCC Fantasy Squad Authentication]'
     FLASKY_MAIL_SENDER = 'Fantasy Squad Admin <fantasysquad30@gmail.com>'
     FLASKY_ADMIN = 'fantasysquad30@gmail.com' #os.environ.get('FLASKY_ADMIN')
